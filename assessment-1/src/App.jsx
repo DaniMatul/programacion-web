781214a5d975345af28aa0721e9e848e1bfc3b71
Daniela Matul <mayda.matul@gmail.com>
2025-08-17 21:08:35 -0600

    feat: add last design of PokemonCard

--- assessment-1/src/App.jsx
+++ assessment-1/src/App.jsx
@@ -3,10 +3,22 @@ import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
+  // ID escogidos por mi :)
+  const ids = [25, 36, 37, 38, 39, 43, 53, 54, 57, 2]
 
   return (
-    <>
-      <PokemonCard/>
+    <> 
+      <div className="container-app">
+        <div className="header">
+
+        </div>
+        <div className="body">
+          {ids.map((i) =>(
+            <PokemonCard id={i}/>
+          ))}
+
+        </div>
+      </div>
     </>
   )
 }
--- assessment-1/src/components/PokemonCard.jsx
+++ assessment-1/src/components/PokemonCard.jsx
@@ -1,29 +1,78 @@
+import { useEffect, useState} from 'react'
 import '../styles/PokemonCard.css'
-export default function PokemonCard(){
-    const pokemon = {
-        name:'Pikachu',
-        num: 127,
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQw3iIiDJQW4HYJk2osK19O3RQ4Hs4MDSF8PQ&s',
-        atack: 100,
-        description: 'Pikachu es muy lindo'
-    }
+import api from '../utils/api'
 
+export default function PokemonCard({id = 25}){
+    const [pokemon, setPokemon] = useState(null);
+
+
+    const getPokemon = async () =>{
+        try{
+            const response = await api.get(`${id}`)
+            const data = response.data
+            setPokemon({
+                name: data.name,
+                ps: data.stats[0].base_stat,
+                image: data.sprites.front_default,
+                num: data.id,
+                weight: data.weight,
+                height: data.height,
+                abilitie: data.abilities[0].ability.name,
+                abilitie2: data.abilities[1].ability.name,
+                type: data.types[0].type.name,
+                atack: data.stats[1].base_stat,
+                defense: data.stats[2].base_stat,
+            })
+
+        }catch(error){
+            alert('No se pudo acceder a PokeApi', error)
+        }
+    };
+
+    useEffect(()=>{
+        getPokemon()
+    }, [id]);
+
+    if (!pokemon) return <></>;
     return (
         <>
             <div className="main-container-pokemon-card">
                 <div className="info-pokemon-card">
-                    <div className="characteristics">
+                    <div className="header-pokemon">
                         <p>{pokemon.name}</p>
-                        <p>{pokemon.num}</p>
+                        <p>{pokemon.ps}</p>
                     </div>
                     <div className="image-pokemon">
                             <img src={pokemon.image} alt="img-pokemon"/>
-                        </div>
-                    <div className="atack">
-                        <p>{pokemon.atack}</p>
+                            <div className="characteristics">
+                                <p> No.{pokemon.num}   Weight:{pokemon.weight /10}kg   Height:{pokemon.height* 10}cm</p>
+                            </div>
                     </div>
-                    <div className="info-extra">
-                        <p>{pokemon.description}</p>
+
+                    <div className="abilities-pokemon">
+                        <p className='title-abilities-pokemon'>Abilities</p>
+                        <div className="text-pokemon">
+                            <p>{pokemon.abilitie}</p>
+                            <p>{pokemon.abilitie2}</p>
+                        </div>
+                    </div>
+
+                    <div className="extra-info-pokemon">
+                        <p className='title-extra-info-pokemon'>Extra Information</p>
+                        <div className="text-pokemon">
+                            <p><strong>Type: </strong></p>
+                            <p>{pokemon.type}</p>
+                        </div>
+                        <div className="text-pokemon">
+                            <p><strong>Atack: </strong></p>
+                            <p>{pokemon.atack}</p>
+                        </div>
+                        <div className="text-pokemon">
+                            <p><strong>Defense: </strong></p>
+                            <p>{pokemon.defense}</p>
+                        </div>
+                        
+                        
                     </div>
                 </div>
             </div>
