68cf1b69355bd88cdacd301c9d9737b104857d33
Daniela Matul <mayda.matul@gmail.com>
2025-10-07 22:49:39 -0600

    feat(models): add Genres as second model

--- books/models.py
+++ books/models.py
@@ -8,3 +8,10 @@ class Author(models.Model):
     
     def __str__(self):
         return self.name
+
+class Genres(models.Model):
+    genre = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.genre
