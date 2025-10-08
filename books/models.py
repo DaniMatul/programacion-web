e8866c1e8ff72a05980a5e04a926fe79a6115035
Daniela Matul <mayda.matul@gmail.com>
2025-10-07 22:47:50 -0600

    feat(models): add author as primary model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200)
+    nationality = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.name
