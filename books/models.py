67c3c7be97e6b3c080f2c751a059af18f49b7b04
Daniela Matul <mayda.matul@gmail.com>
2025-10-07 22:50:51 -0600

    feat(models): create book and add relation between genres and author with book

--- books/models.py
+++ books/models.py
@@ -15,3 +15,12 @@ class Genres(models.Model):
     
     def __str__(self):
         return self.genre
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    genres = models.ManyToManyField(Genres, related_name='books_genre')
+    def __str__(self):
+        return self.title
