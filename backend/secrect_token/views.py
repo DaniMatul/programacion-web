f7db70199822d225ced8991a68bcb6503f6745f3
Daniela Matul <mayda.matul@gmail.com>
2025-10-12 22:50:52 -0600

    feat(api): add Redis connection and endpoints for hiding and revealing secrets

--- backend/secrect_token/views.py
+++ backend/secrect_token/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import redis
+import uuid
+
+
+r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+
+@csrf_exempt
+def hide_secret(request):
+    if request.method == "POST":
+        try:
+            body = json.loads(request.body)
+            secret_text = body.get("text")
+
+            if not secret_text:
+                return JsonResponse({"error": "Missing 'text' field"}, status=400)
+
+            # Crear llave unica
+            key = str(uuid.uuid4())[:8]
+            while r.exists(key):
+                key = str(uuid.uuid4())[:8]
+
+            # Guardar en redis
+            r.set(key, secret_text)
+            return JsonResponse({"key": key})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=500)
+
+    return JsonResponse({"error": "Invalid method"}, status=405)
+
+
+def reveal_secret(request, key):
+    try:
+        if not r.exists(key):
+            return JsonResponse({"error": "Secret not found or already revealed"}, status=404)
+
+        secret = r.get(key)
+        r.delete(key)
+        return JsonResponse({"secret": secret})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=500)
